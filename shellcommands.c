27ad367ab7eb1c3a2a58391cd17dd5f2b321a1a6
Hashim Jacobs <hashjaco@github.com>
2018-10-20 23:59:07 -0700

    Implemented command functions

--- shellcommands.c
+++ shellcommands.c
@@ -12,8 +12,15 @@ int getNumberOfBuiltinCommands(){
 	return sizeof(builtin_commands) / sizeof(char *);
 }
 
-int myshell_cd(char **args){}
-int myshell_ls(char **args){}
+int myshell_cd(char **args){
+	if (args[1] == NULL) fprintf(stderr, "myshell: expected argument to \"cd\"\n");
+	else 
+		if (chdir(args[1]) != 0) perror("Error changing directory\n");
+	return 1;
+}
+int myshell_ls(char **args){
+
+}
 int myshell_pwd(char **args){}
 int myshell_clear(char **args){}
 int myshell_grep(char **args){}
--- shellcommands.h
+++ shellcommands.h
@@ -1,12 +1,14 @@
 #ifndef SHELLCOMMANDS_H
 #define SHELLCOMMANDS_H
 
+int getNumberOfBuiltinCommands();
 int myshell_cd(char **args);
 int myshell_ls(char **args);
 int myshell_pwd(char **args);
 int myshell_clear(char **args);
 int myshell_grep(char **args);
 int myshell_cat(char **args);
+int myshell_exit(char **args);
 
 
 #endif
